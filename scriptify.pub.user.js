b65b17afb24d8d21199e3232cb0c8125e3e4d577
irrationalpie7 <irrationalpie7@gmail.com>
2023-05-05 15:35:52 -0700

    forgot to bump version.

--- scriptify.pub.user.js
+++ scriptify.pub.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AO3 Scriptify
 // @namespace    http://tampermonkey.net/
-// @version      1.0.6
+// @version      1.0.7
 // @description  Color-code dialogue
 // @author       irrationalpie7
 // @match        https://archiveofourown.org/*
