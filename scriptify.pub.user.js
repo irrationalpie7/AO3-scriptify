e80e14987881dd57e2804510c26f3c84d64972bd
irrationalpie7 <irrationalpie7@gmail.com>
2024-01-19 00:42:36 -0800

    Remove unused imports.

--- scriptify.pub.user.js
+++ scriptify.pub.user.js
@@ -1,17 +1,13 @@
 // ==UserScript==
 // @name         AO3 Scriptify
 // @namespace    http://tampermonkey.net/
-// @version      3.1.5
+// @version      3.1.6
 // @description  Color-code dialogue
 // @author       irrationalpie7
 // @match        https://archiveofourown.org/*
 // @updateURL    https://github.com/irrationalpie7/AO3-scriptify/raw/main/scriptify.pub.user.js
-// @downloadURL  https://github.com/Cathalinaheart/AO3-scriptify/raw/main/scriptify.pub.user.js
+// @downloadURL  https://github.com/irrationalpie7/AO3-scriptify/raw/main/scriptify.pub.user.js
 // @require      https://colorjs.io/dist/color.global.min.js
-// @require      https://d3js.org/d3.v4.min.js
-// @require      https://d3js.org/d3-color.v1.min.js
-// @require      https://d3js.org/d3-interpolate.v1.min.js
-// @require      https://d3js.org/d3-scale-chromatic.v1.min.js
 // @require      export.js
 // @require      pick-colors.js
 // @require      color-bar.js
